c87463acc34ea15c9a0efbc2cd1986bb3488c4f5
Maciek <maciejpiwowarczyk@gmail.com>
2018-05-04 22:16:27 +0200

    master abc

--- additional.cs
+++ additional.cs
@@ -10,6 +10,7 @@ namespace HelloWorld
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit. More... Even more...");
             Console.WriteLine("Press any key to exit. More... Even more...");
+			Console.WriteLine("abc");
             Console.ReadKey();
         }
     }

c4b905e63844a68167501a99aee937c9846c01df
Maciek <maciejpiwowarczyk@gmail.com>
2018-05-04 21:28:34 +0200

    additional class

--- hello.cs
+++ hello.cs
@@ -5,11 +5,7 @@ namespace HelloWorld
     {
         static void Main() 
         {
-            Console.WriteLine("Hello World!");
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            AddtionalClass.HelloLogic();
         }
     }
 }
